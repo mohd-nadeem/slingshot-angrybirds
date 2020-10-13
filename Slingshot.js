5ed34906511d1644b9cce71059afab57793c9607
Mohd. Nadeem Sheikh <mohd.nadeem.sheikh24@gmail.com>
2020-10-13 15:57:09 +0530

    Add files via upload
    Slingshot Angry Birds

---
+++ Slingshot.js
@@ -0,0 +1,25 @@
+class Slingshot {
+    constructor(bodyA, pointB) {
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.slingShot = Constraint.create(options);
+        World.add(world, this.slingShot);
+    }
+    fly(){
+        this.slingShot.bodyA = null;
+    }
+
+    display() {
+        if (this.slingShot.bodyA){
+        var pointA = this.slingShot.bodyA.position;
+        var pointB = this.slingShot.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+
+}
